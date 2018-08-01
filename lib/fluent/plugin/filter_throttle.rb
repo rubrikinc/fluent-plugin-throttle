0909eaefb98b857bdeb797394d41c5df120a8dec
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2018-08-01 11:24:19 -0700

    Use v1.0 Plugin API

--- lib/fluent/plugin/filter_throttle.rb
+++ lib/fluent/plugin/filter_throttle.rb
@@ -1,6 +1,6 @@
-require 'fluent/filter'
+require 'fluent/plugin/filter'
 
-module Fluent
+module Fluent::Plugin
   class ThrottleFilter < Filter
     Fluent::Plugin.register_filter('throttle', self)
 
